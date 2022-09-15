4b1567b26b49fffa8700e8f67fef825994fb0d02
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2022-09-15 11:14:13 +0300

    test123

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export const config = {
-  runtime: 'experimental-edge',
-}
+
 
 export const getServerSideProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos')
